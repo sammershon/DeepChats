e62aa1d4e6380d5ac41708f4989dcceee64d0efe
Sam Mershon <samrmershon@gmail.com>
2016-10-07 11:43:27 -0400

    cleaned up formatting

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161005165118) do
+ActiveRecord::Schema.define(version: 20161007144933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 20161005165118) do
     t.datetime "updated_at",                          null: false
     t.string   "last_name"
     t.string   "first_name"
-    t.string   "username"
+    t.string   "username",                            null: false
     t.string   "paradigm"
     t.string   "quote"
     t.string   "book"
